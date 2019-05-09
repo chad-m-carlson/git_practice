f6938fef12d84e5dd5ad70d343c5e7bb2a78b061
Chad Carlson <chad.m.carlson@gmail.com>
2019-05-09 11:31:55 -0600

    colorized and added gems

--- main.rb
+++ main.rb
@@ -1,27 +1,31 @@
+require "colorize"
+
 def puts_git(cmd)
-  puts `git #{cmd} -h` 
+  puts `git #{cmd} -h`
   #BACKTICKS ALLOW YOU TO RUN COMMANDS INSIDE THE TERMINAL
 end
 
 def menu
+  puts "MAIN MENU".colorize(:red)
   puts "1: Enter git Command"
   puts "2: Exit"
   print "> "
   choice = gets.to_i
-  sleep(3) #WAITS () SECONDS AND THEN WILL PERFORM FUNCTION BELOW
   print `clear` #`clear` will clear the terminal
   case choice
   when 1
     puts "Enter git command"
     print "> "
-      puts_git(gets.strip)
-      menu
-    when 2
-      puts "Thanks for using our program!"
-      exit
-    else
-      puts "invalid input"
-      menu
+    puts_git(gets.strip)
+    menu
+  when 2
+    puts "Thanks for using our program!"
+    exit
+  else
+    puts "invalid input"
+    sleep(2) #WAITS () SECONDS AND THEN WILL PERFORM FUNCTION BELOW
+    print `clear`
+    menu
   end
 end
 
